670ed1051d6e73d5afee794ec178fa4dfd4167c1
Daniel Delgado <vennnot@gmail.com>
2024-06-19 14:35:58 +0200

    Dungeon generation conitnues

--- scenes/managers/dungeon_generator/dungeon_generator.gd
+++ scenes/managers/dungeon_generator/dungeon_generator.gd
@@ -15,7 +15,9 @@ var dungeon_grid : Array = []
 var floor_modifier : int = 2
 
 var number_of_rooms : int
+var spawn_room : Room
 
+# this cant be a vector, has to represent the actual rooms
 var last_placed_room : Vector2
 
 enum ROOM_TYPE {ONE,TWO,THREE,FOUR}
@@ -28,9 +30,14 @@ enum ROOM_TYPE {ONE,TWO,THREE,FOUR}
 func _ready() -> void:
 	_initialize_number_of_rooms()
 	_initialize_grid()
-	_generate_spawn_room()
+	#_generate_spawn_room_location()
 	_generate_base_rooms()
-
+	
+	print(dungeon_grid)
+	var room := _get_random_room_type()
+	print(room.shape)
+	find_fit(Vector2.ZERO,room)
+	print(room.shape)
 
 func _initialize_grid() -> void:
 	var modifier := floor_modifier*5
@@ -42,13 +49,13 @@ func _initialize_grid() -> void:
 
 
 #TODO spawn the room
-func _generate_spawn_room() -> void:
+func _generate_spawn_room_location() -> void:
 	@warning_ignore("integer_division")
 	var base_number : Vector2 = Vector2(floor_modifier*5/2,floor_modifier*5/2)
 	base_number.x += (randi_range(-floor_modifier,floor_modifier))
 	base_number.y += (randi_range(-floor_modifier,floor_modifier))
 	dungeon_grid[base_number.x][base_number.y] = 1
-	last_placed_room = dungeon_grid[base_number.x][base_number.y]
+	#last_placed_room = dungeon_grid[base_number.x][base_number.y]
 
 
 
@@ -56,13 +63,10 @@ func _generate_base_rooms() -> void:
 	_initialize_room_chance_variables()
 	while number_of_rooms > 0:
 		var room_to_generate := _get_random_room_type()
-		print(room_to_generate)
 		#TODO connect previous room to current room
 		#TODO update grid
+		#TODO track last placed room
 		number_of_rooms -= 1
-	
-	
-	
 
 
 func _initialize_room_chance_variables() -> void:
@@ -71,41 +75,72 @@ func _initialize_room_chance_variables() -> void:
 	quadruple_room_chance = base_quadruple_room_chance
 
 
-func _get_random_room_type() -> Node2D:
+func _get_random_room_type() -> Room:
 	randomize()
 	var random_float := randf_range(0,1)
+	var room_type : String
 	if random_float < quadruple_room_chance:
-		#FIXME this random ass decrease
+		#FIXME this random ass decrease needs to make more sense
 		quadruple_room_chance /= (8-float(floor_modifier))
-		return room_4
+		room_type = "4"
 	elif random_float < quadruple_room_chance + triple_room_chance:
 		triple_room_chance /= (8-float(floor_modifier)/2)
-		return room_3
+		room_type = "3"
 	elif random_float < quadruple_room_chance + triple_room_chance + double_room_chance:
 		double_room_chance /= (8-float(floor_modifier)/4)
-		return room_2
+		room_type = "2"
 	else:
-		return room_1
+		room_type = "1"
+	
+	return Room.new(room_type)
 
 
 func _initialize_number_of_rooms() -> void:
 	number_of_rooms = 4 + (floor_modifier * 2) + randi_range(0,2)
 
 
-func _get_empty_adjacent_cells() -> Array[Vector2]:
+func _get_empty_adjacent_cells(room:Room) -> Array[Vector2]:
 	var empty_adjacent_cells := []
-	if dungeon_grid[last_placed_room.x+1][last_placed_room.y] == 0:
-		empty_adjacent_cells.append(Vector2(last_placed_room.x+1,last_placed_room.y))
-	elif dungeon_grid[last_placed_room.x-1][last_placed_room.y] == 0:
-		empty_adjacent_cells.append(Vector2(last_placed_room.x-1,last_placed_room.y))
-	elif dungeon_grid[last_placed_room.x][last_placed_room.y+1] == 0:
-		empty_adjacent_cells.append(Vector2(last_placed_room.x,last_placed_room.y+1))
-	elif dungeon_grid[last_placed_room.x+1][last_placed_room.y-1] == 0:
-		empty_adjacent_cells.append(Vector2(last_placed_room.x,last_placed_room.y-1))
+	for exit in room.exits:
+		if dungeon_grid[exit.x][exit.y] == 0:
+			empty_adjacent_cells.append(exit)
 	return empty_adjacent_cells
 
-func _get_fitting_room_orientation() -> void:
-	pass
+
+# Check if the room fits at the given position
+func does_room_fit(room: Room, position: Vector2) -> bool:
+	for cell in room.move_to(position):
+		if not dungeon_grid.has(cell) and dungeon_grid[cell.x][cell.y] != 0:
+			return false
+	return true
+
+
+# Find a fit for the room in a randomized order
+func find_fit(base_position: Vector2, room:Room) -> bool:
+	var max_attempts := 3
+	var attempt := 0
+
+	while attempt < max_attempts:
+		var rotations = [0.0, 90.0, 180.0, 270.0]
+		rotations.shuffle()
+
+		var flips = [false, true]
+		flips.shuffle()
+
+		for rotation in rotations:
+			for flip in flips:
+				room.rotation = 0
+				room.flip_h = false
+				room.rotate(rotation) 
+				room.flip(flip)
+				
+				if does_room_fit(room, base_position):
+					print("Found valid orientation")
+					return true
+		attempt += 1
+
+	print("No possible room could fit after 3 attempts")
+	return false
 
 # generate rooms on a grid
 # 1. Can choose from multiple rooms, repetition and frequency depends on floor difficulty
---
+++ scenes/tools/room_shape/room_shape.gd
@@ -0,0 +1,115 @@
+class_name Room
+
+var shape: Array[Vector2]  # Current cells occupied by the room
+var exits: Array[Vector2]  # Exits relative to the room's position
+var rotation: float = 0 # Current rotation angle
+var flip_h: bool = false # Flipped on the H axis
+var room_type : ROOM_TYPE
+enum ROOM_TYPE {ONE,TWO,THREE,FOUR}
+
+func _init(type:String)-> void:
+	match type:
+		"1":
+			shape.assign(room_shapes["1"])
+			exits.assign(room_exits["1"])
+			room_type = ROOM_TYPE.ONE
+		"2":
+			shape.assign(room_shapes["2"])
+			exits.assign(room_exits["2"])
+			room_type = ROOM_TYPE.TWO
+		"3":
+			shape.assign(room_shapes["3"])
+			exits.assign(room_exits["3"])
+			room_type = ROOM_TYPE.THREE
+		"4":
+			shape.assign(room_shapes["4"])
+			exits.assign(room_exits["4"])
+			room_type = ROOM_TYPE.FOUR
+
+# Apply a rotation to the room
+func rotate(degrees: float) -> void:
+	rotation += degrees
+	rotation = fmod(rotation, 360)
+	
+	var rotated_shape :Array[Vector2]= []
+	for c in shape:
+		rotated_shape.append(rotate_vector(c, degrees))
+		shape = rotated_shape
+	
+	var rotated_exits :Array[Vector2]= []
+	for e in exits:
+		rotated_exits.append(rotate_vector(e, degrees))
+		exits = rotated_exits
+
+	# Apply a flip to the room
+func flip(horizontal: bool)->void:
+	if not horizontal:
+		return
+	
+	if not room_type == ROOM_TYPE.THREE:
+		return
+	
+	flip_h = not flip_h
+	
+	var flipped_shape :Array[Vector2]= []
+	for c in shape:
+		flipped_shape.append(flip_vector_horizontally(c))
+	shape = flipped_shape
+	
+	var flipped_exits :Array[Vector2]= []
+	for e in exits:
+		flipped_exits.append(flip_vector_horizontally(e))
+	exits = flipped_exits
+
+
+func flip_vector_horizontally(v: Vector2) -> Vector2:
+	return Vector2(v.x * -1, v.y)
+
+func rotate_vector(v: Vector2, degrees: float) -> Vector2:
+	var radians = deg_to_rad(degrees)
+	return Vector2(
+		round(v.x * cos(radians) - v.y * sin(radians)),
+		round(v.x * sin(radians) + v.y * cos(radians))
+	)
+
+
+#Translate the room to a new position
+func move_to(position: Vector2) -> Array[Vector2]:
+	var moved_shape :Array[Vector2]= []
+	for cell in shape:
+		moved_shape.append(position + cell)
+	return moved_shape
+
+# Define basic shapes
+var room_shapes = {
+	"1": [
+		Vector2(0, 0)
+	],
+	"4": [
+		Vector2(0, 0), Vector2(1, 0), Vector2(0, 1), Vector2(1, 1)
+	],
+	"2": [
+		Vector2(0, 0), Vector2(1, 0)
+	],
+	"3": [
+		Vector2(0, 0), Vector2(1, 0), Vector2(1, 1)
+	]
+}
+
+var room_exits = {
+	"1": [
+		Vector2(-1, 0), Vector2(1, 0), Vector2(0, -1), Vector2(0, 1)
+	],
+	"4": [
+		Vector2(-1, 0), Vector2(-1, 1), Vector2(0, -1), Vector2(1, -1),
+		Vector2(2, 0), Vector2(2, 1), Vector2(1, 2), Vector2(0, 2)
+	],
+	"2": [
+		Vector2(-1, 0), Vector2(0, -1), Vector2(1, -1),
+		Vector2(2, 0), Vector2(1, 1), Vector2(0, 1)
+	],
+	"3": [
+		Vector2(-1, 0), Vector2(0, -1), Vector2(1, -1),
+		Vector2(2, 0), Vector2(2, 1), Vector2(1, 2), Vector2(0, 1)
+	]
+}
