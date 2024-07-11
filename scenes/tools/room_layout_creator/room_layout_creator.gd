7d6d7929cf4d472e3e4791a929ff055aa46287a7
Daniel Delgado <vennnot@gmail.com>
2024-07-11 07:43:07 +0200

    began mapping exits

--- scenes/tools/room_layout_creator/room_layout_creator.gd
+++ scenes/tools/room_layout_creator/room_layout_creator.gd
@@ -1,30 +1,14 @@
-@tool
 extends Node2D
 
-@export var room_1 : bool :
-	set(value):
-		if value:
-			_reset_other_areas("1")
-		room_1 = value
-@export var room_2 : bool :
-	set(value):
-		if value:
-			_reset_other_areas("2")
-		room_2 = value
-@export var room_3 : bool :
-	set(value):
-		if value:
-			_reset_other_areas("3")
-		room_3 = value
-@export var room_4 : bool :
-	set(value):
-		if value:
-			_reset_other_areas("4")
-		room_4 = value
+var room_1 : bool
+var room_2 : bool
+var room_3 : bool
+var room_4 : bool
 
 var cell_size : int = 16
 var _outline_color := Color(1, 0, 0)
 var _highlight_pos : Vector2 = Vector2.ZERO
+var exits : Dictionary = {}
 
 const room_1_size :=  Vector2(9,9)
 const room_2_size :=  Vector2(18,9)
@@ -32,15 +16,22 @@ const room_3_size :=  Vector2(18,18)
 const room_4_size :=  Vector2(18,18)
 
 
+@onready var room_select_button: OptionButton = %RoomSelectButton
+
+# select what cell to place down and see how much space it takes
+# save and load layouts
+# position cells and save their position + what they are supposed to be
+# mark exits
+# room expands selection to be able to disable a particular exit
+# connect disabling to the actual room
+
+
 func _ready():
-	set_process(true)
+	room_select_button.item_selected.connect(_on_room_selected)
 
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		_update_highlight(get_viewport().get_mouse_position())
-
-
+func _process(_delta: float) -> void:
+	queue_redraw()
 
 func _draw():
 	if room_1:
@@ -51,21 +42,6 @@ func _draw():
 		_draw_l_shape()
 	elif room_4:
 		draw_rect(Rect2(-(room_4_size*cell_size/2), room_4_size*cell_size), _outline_color, false,2)
-	_draw_highlight()
-
-
-func _draw_highlight():
-	var half_size = Vector2(cell_size, cell_size) / 2
-	var adjusted_pos = _highlight_pos
-	
-	# Adjust position based on room conditions
-	if not room_1:
-		adjusted_pos.x += 8
-	if room_3 or room_4:
-		adjusted_pos.y += 8
-	
-	draw_rect(Rect2(adjusted_pos - half_size, Vector2(cell_size, cell_size)), Color(1, 1, 0, 0.5), true)
-
 
 
 func _update_highlight(mouse_pos):
@@ -85,13 +61,33 @@ func _draw_l_shape() -> void:
 	draw_line(Vector2.ZERO, Vector2(-l_shape.x/2,0), _outline_color, 2)
 	draw_line(Vector2(-l_shape.x/2,0), -(l_shape/2), _outline_color, 2)
 
-func _reset_other_areas(active_room:String) -> void:
-	if active_room != "1":
+
+func _on_room_selected(active_room:int) -> void:
+	_initialize_exits(active_room)
+	match active_room:
+		0:
+			room_1 = true
+		1:
+			room_2 = true
+		2:
+			room_3 = true
+		3:
+			room_4 = true
+	active_room += 1
+	if active_room != 1:
 		room_1 = false
-	if active_room != "2":
+	if active_room != 2:
 		room_2 = false
-	if active_room != "3":
+	if active_room != 3:
 		room_3 = false
-	if active_room != "4":
+	if active_room != 4:
 		room_4 = false
 	queue_redraw()
+
+
+func _initialize_exits(active_room:int) -> void:
+	exits = {}
+	print("called")
+	for exit in RoomShape.room_exits[active_room+1]:
+		exits[exit] = "available"
+	print(exits)
