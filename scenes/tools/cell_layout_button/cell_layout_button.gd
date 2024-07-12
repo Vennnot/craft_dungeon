aa78e88bdff65a99cc95da709f2b44303a12c673
Daniel Delgado <vennnot@gmail.com>
2024-07-12 08:39:47 +0200

    cells for rooms

--- scenes/tools/cell_layout_button/cell_layout_button.gd
+++ scenes/tools/cell_layout_button/cell_layout_button.gd
@@ -1,2 +1,8 @@
 extends Button
 class_name CellButton
+
+var id : int = -1
+var vector_location : Vector2 = Vector2.ZERO
+
+func _pressed() -> void:
+	print(vector_location)
--- scenes/tools/room_layout_creator/room_layout_creator.gd
+++ scenes/tools/room_layout_creator/room_layout_creator.gd
@@ -21,7 +21,7 @@ const room_4_size :=  Vector2(18,18)
 @onready var room_cells: Control = %RoomCells
 
 
-var cell_button : PackedScene = preload("res://scenes/tools/cell_layout_button/cell_layout_button.tscn")
+var cell_button_scene : PackedScene = preload("res://scenes/tools/cell_layout_button/cell_layout_button.tscn")
 # select what cell to place down and see how much space it takes
 # save and load layouts
 # position cells and save their position + what they are supposed to be
@@ -76,7 +76,6 @@ func _on_room_selected(active_room:int) -> void:
 			room_3 = true
 		3:
 			room_4 = true
-	_initialize_exits(active_room)
 	active_room += 1
 	if active_room != 1:
 		room_1 = false
@@ -87,15 +86,60 @@ func _on_room_selected(active_room:int) -> void:
 	if active_room != 4:
 		room_4 = false
 	queue_redraw()
+	_clear_current_room()
+	_clear_current_exits()
+	_initialize_exits(active_room)
+	_create_room()
 
 
 func _initialize_exits(active_room:int) -> void:
 	exits = {}
-	for exit in RoomShape.room_exits[active_room+1]:
+	for exit in RoomShape.room_exits[active_room]:
 		exits[exit] = "available"
-	print(exits)
 	_create_exits()
 
+
+func _create_room() -> void:
+	if room_1:
+		_create_room_1()
+	elif room_2:
+		_create_room_2()
+	elif room_3:
+		_create_room_3()
+	elif room_4:
+		_create_room_4()
+
+
+func _create_room_1() -> void:
+	for i in room_1_size.x:
+		for j in room_1_size.y:
+			var cell_button_instance := _instantiate_cell(false)
+			cell_button_instance.vector_location = Vector2(i,j)
+			_place_cell(Vector2((i-room_1_size.x/2)*cell_size,(j-room_1_size.y/2)*cell_size),cell_button_instance)
+
+
+func _create_room_2() -> void:
+	for i in room_2_size.x:
+		for j in room_2_size.y:
+			var cell_button_instance := _instantiate_cell(false)
+			cell_button_instance.vector_location = Vector2(i,j)
+			_place_cell(Vector2((i-room_2_size.x/2)*cell_size,(j-room_2_size.y/2)*cell_size),cell_button_instance)
+
+func _create_room_3() -> void:
+	for i in room_3_size.x:
+		for j in room_3_size.y:
+			if i <= 8 and j >= 9:
+				continue
+			var cell_button_instance := _instantiate_cell(false)
+			cell_button_instance.vector_location = Vector2(i,j)
+			_place_cell(Vector2((i-room_3_size.x/2)*cell_size,(j-room_3_size.y/2)*cell_size),cell_button_instance)
+
+func _create_room_4() -> void:
+	for i in room_4_size.x:
+		for j in room_4_size.y:
+			var cell_button_instance := _instantiate_cell(false)
+			cell_button_instance.vector_location = Vector2(i,j)
+			_place_cell(Vector2((i-room_4_size.x/2)*cell_size,(j-room_4_size.y/2)*cell_size),cell_button_instance)
 
 func _create_exits() -> void:
 	if room_1:
@@ -107,34 +151,77 @@ func _create_exits() -> void:
 	elif room_4:
 		_create_exits_room_4()
 
-
 func _create_exits_room_1() -> void:
-	_place_cell(Vector2(-cell_size/2,(-room_1_size.y*cell_size/2)-cell_size), _instantiate_cell(true))
+	#north and south
+	_place_cell(Vector2(-cell_size/2,(-room_1_size.y*cell_size/2)-cell_size), _instantiate_cell(true,0))
+	_place_cell(Vector2(-cell_size/2,room_1_size.y*cell_size/2), _instantiate_cell(true,2))
+	
+	#east and west
+	_place_cell(Vector2(room_1_size.x*cell_size/2,(-cell_size/2)), _instantiate_cell(true,1))
+	_place_cell(Vector2(-(room_1_size.x+2)*cell_size/2,(-cell_size/2)), _instantiate_cell(true,3))
 
 func _create_exits_room_2() -> void:
-	pass
+	#north and south
+	_place_cell(Vector2(-cell_size*10/2,(-room_2_size.y*cell_size/2)-cell_size), _instantiate_cell(true,0))
+	_place_cell(Vector2(cell_size*10/2,room_2_size.y*cell_size/2), _instantiate_cell(true,3))
+	
+	_place_cell(Vector2(cell_size*10/2,(-room_2_size.y*cell_size/2)-cell_size), _instantiate_cell(true,1))
+	_place_cell(Vector2(-cell_size*10/2,room_2_size.y*cell_size/2), _instantiate_cell(true,4))
+	
+	#east and west
+	_place_cell(Vector2(room_2_size.x*cell_size/2,(-cell_size/2)), _instantiate_cell(true,2))
+	_place_cell(Vector2(-(room_2_size.x+2)*cell_size/2,(-cell_size/2)), _instantiate_cell(true,5))
 
 func _create_exits_room_3() -> void:
-	pass
+		#north and south
+	_place_cell(Vector2(-cell_size*8/2,(-room_3_size.y*cell_size/2)-cell_size), _instantiate_cell(true,0))
+	_place_cell(Vector2(cell_size*8/2,room_3_size.y*cell_size/2), _instantiate_cell(true,4))
+	
+	_place_cell(Vector2(cell_size*8/2,(-room_3_size.y*cell_size/2)-cell_size), _instantiate_cell(true,1))
+	_place_cell(Vector2(-cell_size,(cell_size*8/2)), _instantiate_cell(true,5))
+	
+	#east and west
+	_place_cell(Vector2(room_3_size.x*cell_size/2,(cell_size*8/2)), _instantiate_cell(true,3))
+	_place_cell(Vector2((-cell_size*8/2),0), _instantiate_cell(true,6))
+	
+	_place_cell(Vector2(room_3_size.x*cell_size/2,(-cell_size*8/2)), _instantiate_cell(true,2))
+	_place_cell(Vector2(-(room_3_size.x+2)*cell_size/2,(-cell_size*8/2)), _instantiate_cell(true,7))
 
 func _create_exits_room_4() -> void:
-	pass
+	#north and south
+	_place_cell(Vector2(-cell_size*8/2,(-room_4_size.y*cell_size/2)-cell_size), _instantiate_cell(true,0))
+	_place_cell(Vector2(cell_size*8/2,room_4_size.y*cell_size/2), _instantiate_cell(true,4))
+	
+	_place_cell(Vector2(cell_size*8/2,(-room_4_size.y*cell_size/2)-cell_size), _instantiate_cell(true,1))
+	_place_cell(Vector2(-cell_size*8/2,room_4_size.y*cell_size/2), _instantiate_cell(true,5))
+	
+	#east and west
+	_place_cell(Vector2(room_4_size.x*cell_size/2,(cell_size*8/2)), _instantiate_cell(true,3))
+	_place_cell(Vector2(-(room_4_size.x+2)*cell_size/2,(cell_size*8/2)), _instantiate_cell(true,6))
+	
+	_place_cell(Vector2(room_4_size.x*cell_size/2,(-cell_size*8/2)), _instantiate_cell(true,2))
+	_place_cell(Vector2(-(room_4_size.x+2)*cell_size/2,(-cell_size*8/2)), _instantiate_cell(true,7))
 
 
-func _instantiate_cell(exit:bool) -> CellButton:
-	var cell_button_instance : CellButton = cell_button.instantiate()
+func _instantiate_cell(exit:bool, id=-1) -> CellButton:
+	var cell_button_instance : CellButton = cell_button_scene.instantiate()
 	if exit:
 		exit_cells.add_child(cell_button_instance)
 	else:
 		room_cells.add_child(cell_button_instance)
+	if id != -1:
+		cell_button_instance.id = id
 	return cell_button_instance
 
 
 func _place_cell(pos:Vector2,cell:CellButton) -> void:
 	cell.position = pos
-	print(pos)
 
 
 func _clear_current_exits() -> void:
 	for child in exit_cells.get_children():
+		child.queue_free()
+
+func _clear_current_room() -> void:
+	for child in room_cells.get_children():
 		child.queue_free()
